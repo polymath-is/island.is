b1f8835d34187e82451a6608a253dc03bd641018
Sigurgeir Guðmundsson <sigurgeirg@localhost.localdomain>
2020-11-14 18:17:21 +0000

    vehicle index ip

--- apps/skilavottord/ws/src/app/modules/vehicle/vehicle.module.ts
+++ apps/skilavottord/ws/src/app/modules/vehicle/vehicle.module.ts
@@ -4,7 +4,7 @@ import { VehicleModel } from './model'
 import { VehicleService } from './vehicle.service'
 import { VehicleResolver } from './vehicle.resolver'
 import { RecyclingRequestModel } from '../recycling.request/model'
-import { RecyclingPartnerModel } from '../recycling.partner/model'
+import { RecyclingPartnerModel } from '../recycling.partner'
 
 @Module({
   imports: [
