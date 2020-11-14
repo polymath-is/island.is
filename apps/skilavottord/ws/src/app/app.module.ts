ad50b5edd58ae125f0a4b1dde9c6ac9b223b70dd
Sigurgeir Guðmundsson <sigurgeirg@localhost.localdomain>
2020-11-14 18:10:27 +0000

    index module

--- apps/skilavottord/ws/src/app/app.module.ts
+++ apps/skilavottord/ws/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { Module } from '@nestjs/common'
 import { GraphQLModule } from '@nestjs/graphql'
-import { AuthModule } from './modules/auth'
-import { UserModule } from './modules'
-import { GdprDbModule } from './modules/gdpr/gdpr.module'
 import { SequelizeModule } from '@nestjs/sequelize'
 import { SequelizeConfigService } from './sequelizeConfig.service'
-import { RecyclingPartnerDbModule } from './modules/recycling.partner/recycling.partner.module'
-import { VehicleModule } from './modules/vehicle/vehicle.module'
-import { RecyclingRequestModule } from './modules/recycling.request/recycling.request.module'
-import { VehicleOwnerModule } from './modules/vehicle.owner/vehicle.owner.module'
-import { SamgongustofaModule } from './modules/samgongustofa/samgongustofa.module'
-import { FjarsyslaModule } from './modules/fjarsysla/fjarsysla.module'
+import {
+  AuthModule,
+  UserModule,
+  GdprDbModule,
+  RecyclingPartnerDbModule,
+  VehicleModule,
+  RecyclingRequestModule,
+  VehicleOwnerModule,
+  SamgongustofaModule,
+  FjarsyslaModule,
+} from './modules'
 
 const debug = process.env.NODE_ENV === 'development'
 const playground = debug || process.env.GQL_PLAYGROUND_ENABLED === 'true'
